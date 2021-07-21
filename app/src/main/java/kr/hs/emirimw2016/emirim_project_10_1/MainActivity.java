bb166900ec286dfdf00a782ad886ccfd77cb98d1
user <w2016@e-mirim.hs.kr>
2021-07-21 10:14:44 +0900

    Init Change ViewFlipper Commit

--- app/src/main/java/kr/hs/emirimw2016/emirim_project_10_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirimw2016/emirim_project_10_1/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+// Change ViewFlipper
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup rg;
